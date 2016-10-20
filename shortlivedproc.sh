eb93a2764a69378631067f1f7f641c370b965a37
Alban Crequy <alban@kinvolk.io>
2016-10-20 17:54:00 +0200

    run the script in a loop

--- shortlivedproc.sh
+++ shortlivedproc.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-PORT=$RANDOM
-let "PORT %= 1000"
-let "PORT += 50000"
+function short_conn {
+	PORT=$RANDOM
+	let "PORT %= 1000"
+	let "PORT += 50000"
 
-echo "PORT=$PORT"
-nc -l $PORT &
-sleep 0.2
+	echo "PORT=$PORT"
+	nc -l $PORT &
+	sleep 0.2
 
-echo Hello | nc 127.0.0.1 $PORT
+	echo Hello | nc 127.0.0.1 $PORT
 
-wait
+	wait
+}
 
-echo Done.
-sleep 10000
+while sleep 10 ; do
+	short_conn
+	date
+done
+
